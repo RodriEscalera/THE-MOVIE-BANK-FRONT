8863ad061f6daeb58ebc6214edb5f75daef2753b
Rodriix99 <roescal347@gmail.com>
2023-01-20 19:03:58 -0300

    theming

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,15 @@ import {
   AppBar,
   Box,
   Button,
+  createTheme,
   MenuItem,
-  Select,
+  ThemeProvider,
   Toolbar,
-  Typography,
 } from "@mui/material";
 import Menu from "@mui/material/Menu";
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import FormControl from "@mui/material/FormControl";
 function NavBar() {
   const user = useSelector((state) => state.user);
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -61,34 +60,35 @@ function NavBar() {
             >
               {user.name}
             </Button>
+            <ThemeProvider theme={theme}>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+                sx={{
+                  "& .css-1poimk-MuiPaper-root-MuiMenu-paper-MuiPaper-root-MuiPopover-paper":
+                    {
+                      backgroundColor: "#24D26F",
+                    },
+                }}
+              >
+                <Link to="/profile">
+                  <MenuItem sx={{ color: "black" }} onClick={handleClose}>
+                    Profile
+                  </MenuItem>
+                </Link>
 
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-              sx={{
-                "& .css-1poimk-MuiPaper-root-MuiMenu-paper-MuiPaper-root-MuiPopover-paper":
-                  {
-                    backgroundColor: "#24D26F",
-                  },
-              }}
-            >
-              <Link to="/profile">
-                <MenuItem sx={{ color: "black" }} onClick={handleClose}>
-                  Profile
-                </MenuItem>
-              </Link>
-
-              <Link to="/logout">
-                <MenuItem sx={{ color: "black" }} onClick={handleClose}>
-                  Logout
-                </MenuItem>
-              </Link>
-            </Menu>
+                <Link to="/logout">
+                  <MenuItem sx={{ color: "black" }} onClick={handleClose}>
+                    Logout
+                  </MenuItem>
+                </Link>
+              </Menu>
+            </ThemeProvider>
           </>
         )}
       </Toolbar>
@@ -98,56 +98,10 @@ function NavBar() {
 
 export default NavBar;
 
-/*
-
-
-
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-
-export default function BasicMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <div>
-      <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        Dashboard
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-    </div>
-  );
-}
-
-
-
-
-
-
-*/
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#24D26F",
+    },
+  },
+});
